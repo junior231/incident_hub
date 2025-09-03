aa77ccb55c0d931d765ad7a0d1243012539069d2
junior231 <ilocollins231@gmail.com>
2025-09-02 22:28:25 -0400

    Update next config  for vercel deployment.

--- web/incident-hub-web/next.config.ts
+++ web/incident-hub-web/next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL ||
-      (process.env.NODE_ENV === "production"
-        ? "https://incident-hub.onrender.com"
-        : "http://localhost:8080"),
-  },
 };
 
 export default nextConfig;
-
